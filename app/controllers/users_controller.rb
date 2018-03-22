776c8bc7c4172e7d8c35ad7007077ff9b625c810
Joseph Gage <josephmillergage@gmail.com>
2018-03-22 15:49:16 +0200

    user sign up form styled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :remember_digest, :admin, :activation_digest, :activated, :activated_at, :reset_digest, :reset_sent_at)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
