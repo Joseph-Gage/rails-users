024438e38cf43840dda3adb5b95f65a9476e6525
Joseph Gage <josephmillergage@gmail.com>
2018-03-23 10:08:11 +0200

    added tests for user signup

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ group :test do
   gem 'shoulda-matchers', '~> 3.1.2'
   gem 'faker', '~> 1.8.7'
   gem 'database_cleaner', '~> 1.6.2'
+  gem 'rails-controller-testing', '~> 1.0.1'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,4 +7,30 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST /users' do
+    let(:valid_params) { { user: { name: 'foobar',
+                                     email: 'foo@bar.com', 
+                                     password: 'password', 
+                                     password_confirmation: 'password' } } }
+    context 'when the request is valid' do
+      before { post '/users', params: valid_params }
+
+      it 'should add a user to the database and render user view' do
+        expect(User.first.name).to eq('foobar')
+        expect(response).to redirect_to('/users/1')
+        follow_redirect!
+        expect(response).to render_template(:show)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/users', params: { user: { name: 'foobar' } } }
+
+      it 'should render new user view with errors' do
+        expect(response).to render_template(:new)
+        expect(response.body).to include('error')
+      end
+    end
+  end
 end
