3b4cf0d66fa7366d080d35319fc2368eabb18f7c
Joseph Gage <josephmillergage@gmail.com>
2018-04-02 11:31:07 +0200

    added sign in flow to go to user page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    respond_to do |format|
+      if user && user.authenticate(params[:session][:password])
+        sign_in user
+        format.html { redirect_to user }
+        format.json { render :show, status: :ok, location: user }
+      else
+        format.html { redirect_to signin_path, notice: 'Invalid email/password combination' }
+        format.json { render json: @user.errors, status: :unauthorized }
+      end
+    end
+  end
+
+  def destroy
+  end
 end
